5e0f420aed283afd10edc495111b3cbc681dae0e
Bennett Lang <84034168+Bennebotix@users.noreply.github.com>
2024-09-10 15:13:23 -0500

    Create serviceworker.js

---
+++ js/serviceworker.js
@@ -0,0 +1,17 @@
+const staticDevCoffee = "dev-coffee-site-v1"
+const assets = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/js/app.js",
+  "/images/coffee1.jpg",
+  "/images/coffee2.jpg",
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
