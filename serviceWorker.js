4257a71d73cc277f9071d4920dbe08e4740d9ea7
Bennett Lang <84034168+Bennebotix@users.noreply.github.com>
2024-09-10 15:16:12 -0500

    Update and rename js/serviceworker.js to serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -15,3 +15,11 @@ self.addEventListener("install", installEvent => {
     })
   )
 })
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
+    })
+  )
+})
