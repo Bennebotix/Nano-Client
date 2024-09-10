6051b9e5b6026bccfd91b9e4db397981b8ba88e8
Bennett Lang <84034168+Bennebotix@users.noreply.github.com>
2024-09-10 15:17:58 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -27,3 +27,12 @@ const showCoffees = () => {
 }
 
 document.addEventListener("DOMContentLoaded", showCoffees)
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err))
+  })
+}
