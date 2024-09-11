79827a6b5863d0a8a00bc2e60b1ebb68e19902c8
Bennett Lang <84034168+Bennebotix@users.noreply.github.com>
2024-09-11 11:30:28 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,36 +6,6 @@ setInterval(() => {
     mobile.value = '';
 })
 
-function listenForWaitingServiceWorker(reg, callback) {
-    function awaitStateChange() {
-        reg.installing.addEventListener('statechange', function () {
-            if (this.state === 'installed') callback(reg);
-        });
-    }
-    if (!reg) return;
-    if (reg.waiting) return callback(reg);
-    if (reg.installing) awaitStateChange();
-    reg.addEventListener('updatefound', awaitStateChange);
-}
-
-// reload once when the new Service Worker starts activating
-var refreshing;
-navigator.serviceWorker.addEventListener('controllerchange',
-    function () {
-        if (refreshing) return;
-        refreshing = true;
-        window.location.reload();
-    }
-);
-function promptUserToRefresh(reg) {
-    // this is just an example
-    // don't use window.confirm in real life; it's terrible
-    if (window.confirm("New version available! OK to refresh?")) {
-        reg.waiting.postMessage('skipWaiting');
-    }
-}
-listenForWaitingServiceWorker(reg, promptUserToRefresh);
-
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
     navigator.serviceWorker
