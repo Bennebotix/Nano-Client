43b53b3cac489c40869b81ecede19d7c800adf44
Bennett Lang <84034168+Bennebotix@users.noreply.github.com>
2024-09-11 13:42:26 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,12 +6,10 @@ if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
     navigator.serviceWorker
       .register("/serviceWorker.js")
-      .then(res => {
-          console.log("service worker registered", res);      
-          if (navigator.onLine) {
-             serviceWorkerRegistration = res;
-          }
-                   })
+      .then(res => {        
+        console.log("service worker registered", res);      
+        serviceWorkerRegistration = res;
+      })
       .catch(err => console.log("service worker not registered", err))
   })
 }
