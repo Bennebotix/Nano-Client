c118306237758f0c733b33c107105c32120f492b
Bennett Lang <84034168+Bennebotix@users.noreply.github.com>
2024-09-11 13:41:30 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,20 +2,16 @@ CLI.init('cli');
 
 var serviceWorkerRegistration;
 
-function registerServiceWorker() {
-  if ("serviceWorker" in navigator) {
-    window.addEventListener("load", function() {
-      navigator.serviceWorker
-        .register("/serviceWorker.js")
-        .then(res => {
-            console.log("service worker registered", res);      
-            if (navigator.onLine) {
-               serviceWorkerRegistration = res;
-            }
-                     })
-        .catch(err => console.log("service worker not registered", err))
-    })
-  }
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then(res => {
+          console.log("service worker registered", res);      
+          if (navigator.onLine) {
+             serviceWorkerRegistration = res;
+          }
+                   })
+      .catch(err => console.log("service worker not registered", err))
+  })
 }
-
-registerServiceWorker();
