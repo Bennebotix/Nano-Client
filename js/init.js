d2ea54f78f6346bb2392e206e2d42a4d39852935
Bennett Lang <84034168+Bennebotix@users.noreply.github.com>
2024-09-16 11:10:34 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -3,8 +3,9 @@ CLI.init('cli');
 if (localStorage.getItem('old') !== true) {
     CLI.run('motd');
     localnStorage.setItem('old', true);
-} else {
+}
+
+if (localStorage.getItem('old') == true) {
     CLI.run('motdslim');
-    alert('yay');
     localnStorage.setItem('old', true);
 }
